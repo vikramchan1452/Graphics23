3a3aa33ecc2cb6ad6f89469ca615d5ec0b5d14b7
arvind <arvind@metamation.com>
2023-06-13 13:43:25 +0530

    Initial commit

---
+++ Complex.cs
@@ -0,0 +1,18 @@
+﻿namespace A25;
+
+public struct Complex {
+   public Complex (double x, double y) => (X, Y) = (x, y);
+   public override string ToString () => $"{X} + i{Y}";
+
+   public double Norm => Math.Sqrt (X * X + Y * Y);
+   public double NormSq => X * X + Y * Y;
+
+   public readonly static Complex Zero = new (0, 0);
+
+   public static Complex operator + (Complex a, Complex b)
+      => new (a.X + b.X, a.Y + b.Y);
+   public static Complex operator * (Complex a, Complex b)
+      => new (a.X * b.X - a.Y * b.Y, a.X * b.Y + a.Y * b.X);
+
+   public readonly double X, Y;
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace A25;
+
+class MyWindow : Window {
+   public MyWindow () {
+      Width = 800; Height = 600;
+      Left = 50; Top = 50;
+      WindowStyle = WindowStyle.None;
+      Image image = new Image () {
+         Stretch = Stretch.None,
+         HorizontalAlignment = HorizontalAlignment.Left,
+         VerticalAlignment = VerticalAlignment.Top,
+      };
+      RenderOptions.SetBitmapScalingMode (image, BitmapScalingMode.NearestNeighbor);
+      RenderOptions.SetEdgeMode (image, EdgeMode.Aliased);
+
+      mBmp = new WriteableBitmap ((int)Width, (int)Height,
+         96, 96, PixelFormats.Gray8, null);
+      mStride = mBmp.BackBufferStride;
+      image.Source = mBmp;
+      Content = image;
+
+      DrawMandelbrot (-0.5, 0, 1);
+   }
+
+   void DrawMandelbrot (double xc, double yc, double zoom) {
+      try {
+         mBmp.Lock ();
+         mBase = mBmp.BackBuffer;
+         int dx = mBmp.PixelWidth, dy = mBmp.PixelHeight;
+         double step = 2.0 / dy / zoom;
+         double x1 = xc - step * dx / 2, y1 = yc + step * dy / 2;
+         for (int x = 0; x < dx; x++) {
+            for (int y = 0; y < dy; y++) {
+               Complex c = new Complex (x1 + x * step, y1 - y * step);
+               SetPixel (x, y, Escape (c));
+            }
+         }
+         mBmp.AddDirtyRect (new Int32Rect (0, 0, dx, dy));
+      } finally {
+         mBmp.Unlock ();
+      }
+   }
+
+   byte Escape (Complex c) {
+      Complex z = Complex.Zero;
+      for (int i = 1; i < 32; i++) {
+         if (z.NormSq > 4) return (byte)(i * 8);
+         z = z * z + c;
+      }
+      return 0;
+   }
+
+   void OnMouseMove (object sender, MouseEventArgs e) {
+      if (e.LeftButton == MouseButtonState.Pressed) {
+         try {
+            mBmp.Lock ();
+            mBase = mBmp.BackBuffer;
+            var pt = e.GetPosition (this);
+            int x = (int)pt.X, y = (int)pt.Y;
+            SetPixel (x, y, 255);
+            mBmp.AddDirtyRect (new Int32Rect (x, y, 1, 1));
+         } finally {
+            mBmp.Unlock ();
+         }
+      }
+   }
+
+   void DrawGraySquare () {
+      try {
+         mBmp.Lock ();
+         mBase = mBmp.BackBuffer;
+         for (int x = 0; x <= 255; x++) {
+            for (int y = 0; y <= 255; y++) {
+               SetPixel (x, y, (byte)x);
+            }
+         }
+         mBmp.AddDirtyRect (new Int32Rect (0, 0, 256, 256));
+      } finally {
+         mBmp.Unlock ();
+      }
+   }
+
+   void SetPixel (int x, int y, byte gray) {
+      unsafe {
+         var ptr = (byte*)(mBase + y * mStride + x);
+         *ptr = gray;
+      }
+   }
+
+   WriteableBitmap mBmp;
+   int mStride;
+   nint mBase;
+}
+
+internal class Program {
+   [STAThread]
+   static void Main (string[] args) {
+      Window w = new MyWindow ();
+      w.Show ();
+      Application app = new Application ();
+      app.Run ();
+   }
+}
