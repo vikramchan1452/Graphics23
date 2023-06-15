3cc396d59707d317b67946194a89345db4715cbe
arvind <arvind@metamation.com>
2023-06-15 17:36:47 +0530

    Added GrayBMP class

---
+++ GrayBMP.cs
@@ -0,0 +1,99 @@
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+namespace A25;
+
+#region class GrayBitmap -------------------------------------------------------
+/// <summary>Implements a writeable grayscale bitmap</summary>
+class GrayBMP {
+   #region Constructor --------------------------------------
+   /// <summary>Constructs a grayscale (8 bits-per-pixel) bitmap of given size</summary>
+   public GrayBMP (double width, double height) {
+      mBmp = new WriteableBitmap (mWidth = (int)width, mHeight = (int)height, 96, 96, PixelFormats.Gray8, null);
+      mStride = mBmp.BackBufferStride;
+      mBuffer = mBmp.BackBuffer;
+   }
+   #endregion
+
+   #region Properties ---------------------------------------
+   /// <summary>The underlying WriteableBitmap</summary>
+   public WriteableBitmap Bitmap => mBmp;
+
+   /// <summary>Pointer to the bitmap's buffer - you can obtain this only after a Begin</summary>
+   public nint Buffer {
+      get {
+         if (mcLocks == 0) Fatal ("Buffer access outside Begin() / End()");
+         return mBuffer;
+      }
+   }
+
+   /// <summary>Height of the bitmap, in pixels</summary>
+   public int Height => mHeight;
+
+   /// <summary>The back-buffer stride for this bitmap</summary>
+   public int Stride => mStride;
+
+   /// <summary>Width of the bitmap, in pixels</summary>
+   public int Width => mWidth;
+   #endregion
+
+   #region Methods -----------------------------------------
+   /// <summary>Call Begin before you obtain the Buffer to update the bitmap</summary>
+   public nint Begin () {
+      if (mcLocks++ == 0) {
+         mBmp.Lock ();
+         mX0 = mY0 = int.MaxValue;
+         mX1 = mY1 = int.MinValue;
+      }
+      return mBmp.BackBuffer;
+   }
+
+   /// <summary>Tags a pixel as dirty</summary>
+   public void Dirty (int x, int y) {
+      if (x < mX0) mX0 = x; if (x > mX1) mX1 = x;
+      if (y < mY0) mY0 = y; if (y > mY1) mY1 = y;
+   }
+
+   /// <summary>Tags a rectangle as dirty (x1, x2, y1, y2 need not be 'ordered')</summary>
+   public void Dirty (int x1, int y1, int x2, int y2) {
+      Dirty (x1, y1); Dirty (x2, y2);
+   }
+
+   /// <summary>Call End after finishing the update of the bitmap</summary>
+   public void End () {
+      if (--mcLocks == 0) {
+         if (mcLocks < 0) Fatal ("Unexpected call to GrayBitmap.End()");
+         if (mX1 >= mX0 && mY1 >= mY0)
+            mBmp.AddDirtyRect (new Int32Rect (mX0, mY0, mX1 - mX0 + 1, mY1 - mY0 + 1));
+         mBmp.Unlock (); 
+      }
+   }
+
+   /// <summary>Set a given pixel to a shade of gray</summary>
+   public void SetPixel (int x, int y, int gray) {
+      Check (x, y); Dirty (x, y);
+      var ptr = Begin () + y * mStride + x;
+      unsafe { *(byte*)ptr = (byte)gray; };
+      End ();
+   }
+   #endregion
+
+   #region Implementation ----------------------------------
+   void Check (int x, int y) {
+      if (x < 0 || x >= mWidth || y < 0 || y >= mHeight)
+         Fatal ($"Pixel location out of range: ({x},{y})");
+   }
+
+   // Helper to throw an exception on invalid usage
+   void Fatal (string message)
+      => throw new InvalidOperationException (message);
+
+   readonly int mWidth, mHeight, mStride;
+   readonly WriteableBitmap mBmp;
+   readonly nint mBuffer;
+   int mX0, mY0, mX1, mY1;    // The 'dirty rectangle'
+   int mcLocks;               // Number of unmatched Begin() calls
+   #endregion
+}
+#endregion
--- Program.cs
+++ Program.cs
@@ -11,39 +11,36 @@ class MyWindow : Window {
       Width = 800; Height = 600;
       Left = 50; Top = 50;
       WindowStyle = WindowStyle.None;
-      Image image = new Image () {
+      mBmp = new GrayBMP (Width, Height);
+
+      Image image = new () {
          Stretch = Stretch.None,
          HorizontalAlignment = HorizontalAlignment.Left,
          VerticalAlignment = VerticalAlignment.Top,
+         Source = mBmp.Bitmap
       };
       RenderOptions.SetBitmapScalingMode (image, BitmapScalingMode.NearestNeighbor);
       RenderOptions.SetEdgeMode (image, EdgeMode.Aliased);
-
-      mBmp = new WriteableBitmap ((int)Width, (int)Height,
-         96, 96, PixelFormats.Gray8, null);
-      mStride = mBmp.BackBufferStride;
-      image.Source = mBmp;
       Content = image;
+
 
       DrawMandelbrot (-0.5, 0, 1);
    }
 
    void DrawMandelbrot (double xc, double yc, double zoom) {
       try {
-         mBmp.Lock ();
-         mBase = mBmp.BackBuffer;
-         int dx = mBmp.PixelWidth, dy = mBmp.PixelHeight;
+         mBmp.Begin ();
+         int dx = mBmp.Width, dy = mBmp.Height;
          double step = 2.0 / dy / zoom;
          double x1 = xc - step * dx / 2, y1 = yc + step * dy / 2;
          for (int x = 0; x < dx; x++) {
             for (int y = 0; y < dy; y++) {
-               Complex c = new Complex (x1 + x * step, y1 - y * step);
-               SetPixel (x, y, Escape (c));
+               Complex c = new (x1 + x * step, y1 - y * step);
+               mBmp.SetPixel (x, y, Escape (c));
             }
          }
-         mBmp.AddDirtyRect (new Int32Rect (0, 0, dx, dy));
       } finally {
-         mBmp.Unlock ();
+         mBmp.End ();
       }
    }
 
@@ -56,46 +53,7 @@ class MyWindow : Window {
       return 0;
    }
 
-   void OnMouseMove (object sender, MouseEventArgs e) {
-      if (e.LeftButton == MouseButtonState.Pressed) {
-         try {
-            mBmp.Lock ();
-            mBase = mBmp.BackBuffer;
-            var pt = e.GetPosition (this);
-            int x = (int)pt.X, y = (int)pt.Y;
-            SetPixel (x, y, 255);
-            mBmp.AddDirtyRect (new Int32Rect (x, y, 1, 1));
-         } finally {
-            mBmp.Unlock ();
-         }
-      }
-   }
-
-   void DrawGraySquare () {
-      try {
-         mBmp.Lock ();
-         mBase = mBmp.BackBuffer;
-         for (int x = 0; x <= 255; x++) {
-            for (int y = 0; y <= 255; y++) {
-               SetPixel (x, y, (byte)x);
-            }
-         }
-         mBmp.AddDirtyRect (new Int32Rect (0, 0, 256, 256));
-      } finally {
-         mBmp.Unlock ();
-      }
-   }
-
-   void SetPixel (int x, int y, byte gray) {
-      unsafe {
-         var ptr = (byte*)(mBase + y * mStride + x);
-         *ptr = gray;
-      }
-   }
-
-   WriteableBitmap mBmp;
-   int mStride;
-   nint mBase;
+   GrayBMP mBmp;
 }
 
 internal class Program {
