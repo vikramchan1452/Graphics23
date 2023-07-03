bd0d38ae312a1387318040ffd46c90eeb20acfa3
arvind <arvind@metamation.com>
2023-07-03 18:39:24 +0530

    Multithreaded Mandelbrot

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Windows.Media;
 using System.Windows.Input;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using System.Collections.Concurrent;
 
 namespace A25;
 
@@ -23,31 +25,97 @@ class MyWindow : Window {
       RenderOptions.SetEdgeMode (image, EdgeMode.Aliased);
       Content = image;
 
+      DispatcherTimer timer = new DispatcherTimer () {
+         Interval = TimeSpan.FromMilliseconds (30), IsEnabled = true,
+      };
+      timer.Tick += NextFrame;
+   }
 
-      DrawMandelbrot (-0.5, 0, 1);
+   void NextFrame (object? sender, EventArgs e) {
+      DrawMandelbrotThread (-0.59990625, -0.42907020097, mScale);
+      mScale *= 1.02;
+   }
+   double mScale = 1000;
+
+   void DrawMandelbrotRow (int y) {
+      unsafe {
+         byte* ptr = (byte*)(mBase + y * mStride);
+         for (int x = 0; x < mDX; x++) {
+            Complex c = new (mX1 + x * mStep, mY1 - y * mStep);
+            *ptr++ = Escape (c);
+         }
+      }
    }
 
    void DrawMandelbrot (double xc, double yc, double zoom) {
       try {
          mBmp.Begin ();
-         int dx = mBmp.Width, dy = mBmp.Height;
-         double step = 2.0 / dy / zoom;
-         double x1 = xc - step * dx / 2, y1 = yc + step * dy / 2;
-         for (int x = 0; x < dx; x++) {
-            for (int y = 0; y < dy; y++) {
-               Complex c = new (x1 + x * step, y1 - y * step);
-               mBmp.SetPixel (x, y, Escape (c));
-            }
-         }
+         mBase = mBmp.Buffer;
+         mStride = mBmp.Stride;
+         mDX = mBmp.Width; mDY = mBmp.Height;
+         mStep = 2.0 / mDY / zoom;
+         mX1 = xc - mStep * mDX / 2; mY1 = yc + mStep * mDY / 2;
+
+         int threads = 50;
+         mQueue.Clear ();
+         for (int i = 0; i < mDY; i++) mQueue.Enqueue (i);
+         List<Task> tasks = new List<Task> ();
+         for (int i = 0; i < threads; i++)
+            tasks.Add (Task.Run (TaskProc));
+         Task.WaitAll (tasks.ToArray ());
+         mBmp.Dirty (0, 0, mDX - 1, mDY - 1);
+
       } finally {
          mBmp.End ();
       }
    }
 
+   void TaskProc () {
+      while (mQueue.TryDequeue (out int y))
+         DrawMandelbrotRow (y);
+   }
+
+   void DrawMandelbrotThread (double xc, double yc, double zoom) {
+      try {
+         mBmp.Begin ();
+         mBase = mBmp.Buffer;
+         mStride = mBmp.Stride;
+         mDX = mBmp.Width; mDY = mBmp.Height;
+         mStep = 2.0 / mDY / zoom;
+         mX1 = xc - mStep * mDX / 2; mY1 = yc + mStep * mDY / 2;
+
+         int threads = 50;
+         mQueue.Clear ();
+         for (int i = 0; i < mDY; i++) mQueue.Enqueue (i);
+         mCEV = new CountdownEvent (threads);
+         for (int i = 0; i < threads; i++) {
+            Thread t1 = new Thread (ThreadProc);
+            t1.Start ();
+         }
+         mCEV.Wait ();
+         mBmp.Dirty (0, 0, mDX - 1, mDY - 1);
+
+      } finally {
+         mBmp.End ();
+      }
+   }
+
+   double mX1, mY1, mStep;
+   nint mBase;
+   int mYNext, mStride, mDX, mDY;
+   ConcurrentQueue<int> mQueue = new ConcurrentQueue<int> ();
+   CountdownEvent mCEV;
+
+   void ThreadProc () {
+      while (mQueue.TryDequeue (out int y))
+         DrawMandelbrotRow (y);
+      mCEV.Signal ();
+   }
+
    byte Escape (Complex c) {
       Complex z = Complex.Zero;
-      for (int i = 1; i < 32; i++) {
-         if (z.NormSq > 4) return (byte)(i * 8);
+      for (int i = 1; i < 255; i++) {
+         if (z.NormSq > 4) return (byte)i;
          z = z * z + c;
       }
       return 0;
